64ede8bcbb5c4b7d4e44fc0b39dc430f5a8b307d
최성원 <chesw516@jbnu.ac.kr>
2025-11-28 00:07:40 +0900

    cctv addition

--- db/init.sql
+++ db/init.sql
@@ -16,8 +16,14 @@ INSERT INTO cctv_streams (id, name, stream_url, latitude, longitude) VALUES
     37.504058, 127.024304),
     ('4', '논현역', 'https://strm2.spatic.go.kr/live/206.stream/playlist.m3u8', 
     37.510832, 127.021142)
-        ('4', '논현역', 'https://strm2.spatic.go.kr/live/206.stream/playlist.m3u8', 
-    37.510832, 127.021142)
+    ('5', '역삼역', 'https://strm4.spatic.go.kr/live/338.stream/playlist.m3u8', 
+    37.500696, 127.036562)
+    ('6', '양재역', 'https://strm2.spatic.go.kr/live/208.stream/playlist.m3u8', 
+    37.484350, 127.033936)
+    ('7', '서초대로', 'https://strm2.spatic.go.kr/live/206.stream/playlist.m3u8', 
+    37.497135, 127.024347) -- url 수정 필요(현재 논현역과 동일)
+    ('8', '교대역', 'https://strm3.spatic.go.kr/live/318.stream/playlist.m3u8', 
+    37.493891, 127.013694)
 
 ON CONFLICT (id) DO NOTHING;
 
